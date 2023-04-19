3d029082e4b80cf84c2750130d6e17d1eabd0997
simoninic <112722314+simoninic@users.noreply.github.com>
2023-04-19 11:24:25 -0400

    Add files via upload

---
+++ cnv_hmm.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+### General Outline
+# Read and grab the CNV data (similar to Q3 of HW4)
+# Use HMMs to predict the states for affecting CNV
